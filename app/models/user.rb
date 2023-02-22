cefb7748d55d454de9d146fcc585c7c82515635e
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2023-02-22 15:07:25 +0500

    added fog gem and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :password, presence: true
 end
