3ba3ba88647a3604ff97e7d71c9ccbed8f7b1afc
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2023-02-22 15:02:58 +0500

    small tweaks

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
-    config.storage = :fog
     provider: 'Google',
     google_storage_access_key_id: ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
     google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
