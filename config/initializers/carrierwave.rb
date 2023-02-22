939829f489ab82431c30d7187d7de1bc40b7beee
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2023-02-22 15:12:54 +0500

    added fog gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,8 @@
 CarrierWave.configure do |config|
-  config.storage = :fog
   config.fog_credentials = {
     provider: 'Google',
     google_storage_access_key_id: ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
     google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
   }
   config.fog_directory = 'slice-app'
-  config.fog_public = false
 end
