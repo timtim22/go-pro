3473811c01b7c2bb1c3e2fe871bd3c706b249e78
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2023-02-27 22:16:55 +0500

    small tweaks in video creation

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -27,10 +27,10 @@ class VideosController < ApplicationController
 
   def create
     @video = @current_user.videos.new(video_params)
-    tempfile = params[:file].tempfile
+    file = params[:file]
     # binding.pry
     # binding.remote_pry
-    file_url = upload_file_to_cloud_storage(tempfile)
+    file_url = upload_file_to_cloud_storage(file)
     VideoProcessWorker.perform_async(file_url, JSON.parse(params[:file].to_json), @current_user.id)
     json_success("Your video is being uploaded. Once the upload is complete, you will find it in the 'My Library > Recent Videos' section.")
   end
